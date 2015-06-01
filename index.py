6d7e891086cb666e64d694122c9f64dc2b0400d8
Gautam Krishnan <cgautamkrish@gmail.com>
2015-06-01 13:20:06 +0800

    changes to getProbability(). Ought to work but only giving valid answers for 4 lines; all 4 are correct though

--- index.py
+++ index.py
@@ -1,21 +1,18 @@
 import codecs
 import nltk
 import collections
+import operator
 
 def getProbability(lang_list, key):
 	counter = collections.Counter(lang_list)
-	length_ngrams = len(counter.values()) 
-	list_of_probs = [(i+1) for i in counter.values()]
-	probability_list = [(i/float(length_ngrams)) for i in list_of_probs]
-	with open("prob.txt", "a") as g:
-		g.write(str(length_ngrams) + ' : ' + key)
-		g.write('\n')
-		count = 0
-		for l,k in counter.items():
-			count += 1
-		g.write(str(count))
-		g.write('\n')
-	return probability_list
+	total_count = 0
+	new_dict = {}
+	for i in counter:
+		total_count += counter[i]
+	for g in counter:
+		prob = float(counter[g]+1)/total_count
+		new_dict[g] = prob
+	return new_dict
 
 def getFileContents(file_name):
 	file_content = codecs.open(file_name, "r", "utf-8")
@@ -26,8 +23,8 @@ def getNgrams(line):
 	tokens = []
 	for i in range(0, length):
 		ngram = ()
-		if (i + 3) < length:
-			ngram = (line[i], line[i+1], line[i+2], line[i+3])
+		if (i + 2) < length:
+			ngram = (line[i], line[i+1], line[i+2])
 			tokens.append(ngram)
 	return tokens
 
@@ -63,22 +60,21 @@ for key,value in lang_dict.items():
 				pass
 
 # Get probabilites for each language
-indon = []
-malay = []
-tam = []
-othe = []
+indon = {}
+malay = {}
+tam = {}
+othe = {}
 
 lang_probabilities = {'indonesian' : indon, 'malaysian' : malay, 'tamil' : tam, 'others' : othe}
-for key in lang_dict:
-	# print(lang_dict[key])
+for key in lang_probabilities:
 	lang_probabilities[key] = getProbability(lang_dict[key], key)
-	with open('langs.txt', 'a') as f:
-		for k in lang_dict[key]:
-			try:
-				f.write(' '.join(k))
-				f.write(' ')
-			except UnicodeEncodeError:
-				pass
+	# with open('langs.txt', 'a') as f:
+	# 	for k in lang_probabilities[key]:
+	# 		try:
+	# 			f.write(' '.join(k))
+	# 			f.write(' ')
+	# 		except UnicodeEncodeError:
+	# 			pass
 
 # for i in lang_probabilities:
 # 	print(lang_probabilities[i])
@@ -88,31 +84,19 @@ test_file_contents = getFileContents('input.test.txt')
 
 for line in test_file_contents:
 	try:
-		probability = {'indonesian' : 1, 'malaysian' : 1, 'tamil' : 1, 'others' : 1}
+		probability = {'indonesian' : 0, 'malaysian' : 0, 'tamil' : 0, 'others' : 0}
 		tokens = nltk.word_tokenize(line)
 		line_ngrams = getNgrams(tokens)
-		# print(line_ngrams)
 		for i in line_ngrams:
-			for key,value in lang_dict.items():
-				for idx,item in enumerate(value):
+			for j in lang_probabilities:
+				for item in lang_probabilities[j]:
 					if i == item:
-						print(i)
-						probability[key] = probability[key] * lang_probabilities[key][idx]
-		print(probability)
-
-		# with open("all_items.txt", "a") as d:
-		# 	for single_ngram in line_ngrams:
-		# 		print(single_ngram)
-		# 		with open("test_tokens.txt", "a") as h:
-		# 			h.write(str(single_ngram))
-		# 			h.write('\n')
-		# 		for key,value in lang_dict.items():
-		# 			for idx,item in enumerate(value):
-		# 				d.write(str(item))
-		# 				d.write('\n')
-		# 				if item == single_ngram:
-		# 					#print(item)
-		# 					probability[key] = probability[key] * lang_probabilities[key][idx]
-		# print(probability)
+						if probability[j] == 0:
+							probability[j] = lang_probabilities[j][item]
+						else:
+							probability[j] = probability[j] * lang_probabilities[j][item]
+		likely_language = max(probability.items(), key=operator.itemgetter(1))[0]
+		print("Detected language: " + likely_language)
 	except UnicodeEncodeError:
 		pass
+
